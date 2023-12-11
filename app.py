bd76d4368a56146d06db45a363ad27aeb8073129
Dimas <dimasfaid@gmail.com>
2023-12-11 19:05:43 +0700

    predict image from bucket

--- app.py
+++ app.py
@@ -32,26 +32,9 @@ def update_model():
 def predict():
   # local_image_path = './test/test_ayam_goreng.jpg'
 
-  # # Make predictions for the local image
-  # img = image.load_img(local_image_path, target_size=(224, 224))  # Adjust target_size as needed
-  # img_array = image.img_to_array(img)
-  # img_array = np.expand_dims(img_array, axis=0)
-  # img_array = preprocess_input(img_array)
-
-  # # Make predictions
-  # predictions = model.predict(img_array)
-  # # print(predictions * 100)
-
-  # predicted_class = np.argmax(predictions)
-
-  # # print(f"The predicted class is: {predicted_class}")
-  # return str(predicted_class)
-
   """Receive and parse Pub/Sub messages."""
   envelope = request.get_json()
-  print('envelope start')
-  print(envelope)
-  print('envelope end')
+  
   if not envelope:
       msg = "no Pub/Sub message received"
       print(f"error: {msg}")
@@ -64,11 +47,27 @@ def predict():
 
   pubsub_message = envelope["message"]
 
-  name = "World"
+  predicted_class = None
   if isinstance(pubsub_message, dict) and "data" in pubsub_message:
-      name = base64.b64decode(pubsub_message["data"]).decode("utf-8").strip()
+      event_data = base64.b64decode(pubsub_message["data"]).decode("utf-8").strip()
+      name = event_data["name"]
+      bucket = event_data["bucket"]
+      # Make predictions for the local image
+      img = image.load_img("gs://"+bucket+name, target_size=(224, 224))  # Adjust target_size as needed
+      img_array = image.img_to_array(img)
+      img_array = np.expand_dims(img_array, axis=0)
+      img_array = preprocess_input(img_array)
 
-  print(f"Hello {name}!")
+      # Make predictions
+      predictions = model.predict(img_array)
+      # print(predictions * 100)
+
+      predicted_class = np.argmax(predictions)
+
+      # print(f"The predicted class is: {predicted_class}")
+      # return str(predicted_class)
+      
+  print(f"The predicted class is: {predicted_class}")
 
   return ("",204)
 
